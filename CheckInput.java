b5b404dce8e34d2f07720c6023704a085ec087b6
Jaehun_Kim <wogns4488@gmail.com>
2022-03-17 14:15:54 +0900

    객체지향 프로그래밍 3주차 수업(2022-03-17) -반복문(for, while, do~while)에 대해 배웠습니다.

---
+++ CheckInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CheckInput {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	int month;
+	
+	do {
+		System.out.print("올바른 월을 입력해주세요 [1 ~ 12]: ");
+		month = input.nextInt();
+	}while(month < 1 || month > 12);
+	System.out.println("사용자가 입력한 값은: " + month);
+	}
+}
---
+++ WelcomeLoop.java
@@ -0,0 +1,11 @@
+
+public class WelcomeLoop {
+	public static void main(String[] args) {
+		int i = 0;
+		do {
+			System.out.println(i + " Welcome!");
+			i++;
+		}while(i < 0);
+		System.out.println("Quit Loop");
+	}
+}
